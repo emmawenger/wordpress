733de4202fd7e2569f44c7258a7a9e537b4cd0ce
Emma Wenger <emmawenger@Emmas-MacBook-Pro.local>
2017-07-10 17:09:36 -0400

    Social Media

--- wp-content/themes/maintheme/parts/page/social-media.php
+++ wp-content/themes/maintheme/parts/page/social-media.php
@@ -1,10 +1,9 @@
 <section id="social-media">
 	<div class="container">
 		<ul>
-			<li><a href="">Facebook</a></li>
+			<li><a href="https://www.facebook.com/emma.k.wenger" target="_blank">Facebook</a></li>
 			<li><a href="">Twitter</a></li>
-			<li><a href="">Instagram</a></li>
-			<li><a href="">Snapchat</a></li>
+			<li><a href="https://www.instagram.com/e.wenger/" target="_blank">Instagram</a></li>
 		</ul>
 	</div>
 </section>
