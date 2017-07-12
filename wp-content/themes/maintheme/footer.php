25bb3aefecfd698fc0b197a34213c8d79a18bb67
Emma Wenger <emma.wenger97@gmail.com>
2017-07-12 14:14:37 -0400

    Styling 1.0

--- wp-content/themes/maintheme/footer.php
+++ wp-content/themes/maintheme/footer.php
@@ -1,18 +1,13 @@
 <footer>
     <div class="container">
-        <h4>Footer</h4>
+        
         <?php
         $nav_args = array(
             'theme_location' => 'footer-left',
         );
         wp_nav_menu($nav_args);
         ?>
-        <?php
-        $nav_args = array(
-            'theme_location' => 'footer-right',
-        );
-        wp_nav_menu($nav_args);
-        ?>
+    
     </div>
 </footer>
 <?php wp_footer(); ?>
--- wp-content/themes/maintheme/parts/page/social-media.php
+++ wp-content/themes/maintheme/parts/page/social-media.php
@@ -2,7 +2,6 @@
 	<div class="container">
 		<ul>
 			<li><a href="https://www.facebook.com/emma.k.wenger" target="_blank">Facebook</a></li>
-			<li><a href="">Twitter</a></li>
 			<li><a href="https://www.instagram.com/e.wenger/" target="_blank">Instagram</a></li>
 		</ul>
 	</div>
